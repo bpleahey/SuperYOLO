f65ce8dd862d411a36a54fc6dc026827f3bcdac2
Brendan <brendan_leahey@brown.edu>
2023-06-21 16:23:15 +0200

    had a wrong print before

--- utils/datasets.py
+++ utils/datasets.py
@@ -701,7 +701,6 @@ class LoadImagesAndLabels_sr(Dataset):  # for training/testing
         # Check cache
         self.label_files = img2label_paths(self.img_files)  # labels
         self.ir_files = img2ir_paths(self.img_files)
-        print((self.label_files[0]).parent)
         cache_path = Path(self.label_files[0]).parent.with_suffix('.cache')  # cached labels
         if cache_path.is_file():
             cache = torch.load(cache_path)  # load
